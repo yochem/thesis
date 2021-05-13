042a180a453bb6ba835d9c679b550f0ac868a1fa
Yochem van Rosmalen <yochem+git@icloud.com>
2021-05-13 12:43:01 +0200

    Move paper to own folder

--- clean.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-rm *.{aux,bbl,blg,fdb_latexmk,fls,log,out,tdo,synctex.gz,toc}
---
+++ paper/clean.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+rm *.{aux,bbl,blg,fdb_latexmk,fls,log,out,tdo,synctex.gz,toc} 2>/dev/null
