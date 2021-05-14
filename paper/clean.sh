848cae39c4395238f38d800cea9dd5afaea93d7d
Yochem van Rosmalen <yochem+git@icloud.com>
2021-05-14 16:50:31 +0200

    Update related work

--- paper/clean.sh
+++ paper/clean.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/env bash
 
-rm *.{aux,bbl,blg,fdb_latexmk,fls,log,out,tdo,synctex.gz,toc} 2>/dev/null
+extensions=(
+    acn
+    acr
+    alg
+    aux
+    bbl
+    blg
+    fdb_latexmk
+    fls
+    glg
+    glo
+    gls
+    ist
+    log
+    out
+    synctex.gz
+    tdo
+    toc
+)
+
+for i in "${extensions[@]}"; do
+    rm *.$i 2>/dev/null
+done
